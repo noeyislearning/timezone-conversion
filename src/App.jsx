ad9209aceac04929dbb5cfc0647768b00ff57c80
Noey Ignacio <hello@noeyislearning.dev>
2023-12-12 19:54:06 +0800

    feat: add basic rate limiter

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,83 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"
 
 /** Heroicons */
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import { ArrowRightIcon } from "@heroicons/react/24/outline"
 
 /** Components */
-import Button from "./components/buttons/Button";
+import Button from "./components/buttons/Button"
 
 /** API */
-import { convertTimezone } from "./api/timeAPI";
+import { convertTimezone } from "./api/timeAPI"
 
 /** Data (JSON) */
-import buttonsData from "./assets/data/buttons.json";
+import buttonsData from "./assets/data/buttons.json"
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
-  const [pickedTime, setPickedTime] = useState("");
-  const [activeButton, setActiveButton] = useState("");
-  const [convertedTime, setConvertedTime] = useState("");
+  const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString())
+  const [pickedTime, setPickedTime] = useState("")
+  const [activeButton, setActiveButton] = useState("")
+  const [convertedTime, setConvertedTime] = useState("")
+  const [clickCount, setClickCount] = useState(0)
+  const [showWarning, setShowWarning] = useState(false)
+  const [disableButtons, setDisableButtons] = useState(false)
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setCurrentTime(new Date().toLocaleTimeString());
-    }, 1000);
+      setCurrentTime(new Date().toLocaleTimeString())
+    }, 1000)
 
     return () => {
-      clearInterval(timer);
+      clearInterval(timer)
+    }
+  }, [])
+
+  useEffect(() => {
+    let clickTimer;
+    if (clickCount >= 3) {
+      setShowWarning(true);
+      setDisableButtons(true);
+
+      clickTimer = setTimeout(() => {
+        setShowWarning(false);
+        setClickCount(0);
+        setDisableButtons(false);
+      }, 5000); 
+    }
+
+    return () => {
+      clearTimeout(clickTimer);
     };
-  }, []);
+  }, [clickCount]);
 
   const formatTime = (time) => {
-    const date = new Date(time);
-    return date.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', second: '2-digit', hour12: true });
-  };
+    const date = new Date(time)
+    return date.toLocaleTimeString([], { hour: 'numeric', minute: '2-digit', second: '2-digit', hour12: true })
+  }
 
   const handlePickedTime = async (timezone) => {
     try {
-      const picked = new Date().toISOString();
-      setPickedTime(formatTime(picked));
+      const picked = new Date().toISOString()
+      setPickedTime(formatTime(picked))
 
-      const converted = await convertTimezone("Asia/Manila", timezone, picked);
-      setConvertedTime(formatTime(converted));
+      const converted = await convertTimezone("Asia/Manila", timezone, picked)
+      setConvertedTime(formatTime(converted))
     } catch (error) {
-      console.error("Error converting time:", error);
+      console.error("Error converting time:", error)
+    }
+  }
+
+  const handleClick = async (timezone) => {
+    if (!disableButtons) {
+      setActiveButton(timezone);
+      handlePickedTime(timezone);
+
+      setClickCount((prevCount) => prevCount + 1);
     }
   };
 
-  const handleClick = (timezone) => {
-    setActiveButton(timezone);
-    handlePickedTime(timezone);
-  };
-
   return (
-    <main className="flex w-full h-screen items-center justify-center">
-      <div className="flex flex-col items-center gap-4">
+    <main className="w-full h-screen flex items-center justify-center">
+      <div className="w-full flex flex-col items-center gap-4">
         <div className="flex flex-col items-center gap-4">
           <h3 className="text-2xl">Time Conversion</h3>
           <div className="flex flex-col items-center gap-4">
@@ -80,12 +105,18 @@ function App() {
               label={button.label}
               onClick={() => handleClick(button.timezone)}
               isActive={activeButton === button.timezone}
+              disabled={disableButtons}
             />
           ))}
         </div>
+        <div className="mt-2 flex w-full justify-center max-w-lg">
+        { showWarning && (
+          <p className="text-red-500 text-sm">You've reached the limit of button clicks within a second.</p>
+        )}
+        </div>
       </div>
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
